7bf8ddf385de0a919c6330343f21103748de3a6d
Elvis Rafael <rafaelelvis23@gmail.com>
2026-04-21 14:06:46 +0200

    Practica de TailwindCSS realizada desplegada en producción, proyectos React de repaso desde lo más básico

---
+++ Frontend-React/extra-projects/react-first-project/index.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <>
+    <h1>Why am I excited about learning React?</h1>
+    <ul>
+      <li>I can create interactive web applications</li>
+      <li>I can create reusable components</li>
+      <li>I can create single-page applications</li>
+    </ul>
+  </>
+)
